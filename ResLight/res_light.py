8b7adc75aa0caa5afd4c2e154ab9699b99917a54
Ken <wken@sas.upenn.edu>
2017-02-14 18:55:04 -0500

    Implemented hierarchical data structure

--- ResLight/res_light.py
+++ ResLight/res_light.py
@@ -1,5 +1,6 @@
 import os
 from openpyxl import load_workbook
+from operator import itemgetter, attrgetter
 class ResLight:
     def __init__(self, full_filename, source):
         if source is "DOE":
@@ -10,8 +11,9 @@ class ResLight:
             wb = load_workbook(full_filename)
             print("Done")
             tool = wb.get_sheet_by_name("C) Tool")
-            self.light_data = []
-            for row in tool["A3:A16197"]: #16197
+            sort_order = [tool["A2"].value, tool["B2"].value, tool["C2"].value, tool["D2"].value]
+            self.light_data = LightData(sort_order)
+            for row in tool.iter_rows(min_row = 3, max_row = 16197): #16197
                 attributes = {}
                 for cell in row:
                     col = cell.column
@@ -22,15 +24,69 @@ class ResLight:
                         category = tool[col + "2"].value
                         attributes[category] = cell.value
                 light_datum_temp = LightDatum(attributes)
-                self.light_data.append(light_datum_temp)
+                self.light_data.insert(light_datum_temp)
             print(len(self.light_data))
+            self.light_data.attributes = sorted(self.light_data.attributes,
+                                     key = itemgetter(sort_order[0], sort_order[1],
+                                                      sort_order[2], sort_order[3]))
         else:
             raise Exception("Only DOE files accepted at this time")
 
 class LightDatum:
     def __init__(self, attributes):
         self.attributes = attributes
-        
+
+    def compare_to(self, other, sort_attributes):
+        for attr in sort_attributes:
+            if self[attr] != other[attr]:
+                return self[attr] < other[attr]
+        return True
+            
+class LightData:
+    def __init__(self, hierarchy):
+        "hierarchy: list of strings indicating hierarchy levels"
+        self.hierarchy = hierarchy
+        self.data = Category("Master")
+    
+    def insert(self, light_datum):
+        current_category = self.data
+        for element in self.hierarchy:
+            cat_name = light_datum.attributes[element]
+            if current_category.has_child(cat_name):
+                current_category = current_category.get_child(cat_name)
+            else:
+                new_cat = Category(cat_name)
+                current_category.add(new_cat)
+        current_category.add_datum(light_datum)
+
+    def insertion_index(self, light_datum):
+        lbound = 0
+        rbound = len(self.data)
+        if lbound == rbound:
+            return lbound
+
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def add(self, element):
+        "Adds a child category to this category"
+        self.children.append(element)
+
+    def has_child(self, name):
+        for child in self.children:
+            if child.name == name:
+                return True
+        return False
+
+    def get_child(self, name):
+        for child in self.children:
+            if child.name == name:
+                return child
+
+    def add_datum(self, light_datum):
+        self.light_datum = light_datum
 
 r = ResLight('../Data/reslight_DOE_2012.xlsx', 'DOE')
         
